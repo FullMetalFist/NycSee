508ce7de90cd0221b9c8d889810d8c975232f704
Michael Vilabrera <m.vilabrera@gmail.com>
2014-07-26 21:15:39 -0400

    before Apple Doc Autolayout

--- NycSee/Controller/DetailViewController.m
+++ NycSee/Controller/DetailViewController.m
@@ -8,11 +8,30 @@
 
 #import "DetailViewController.h"
 
-@interface DetailViewController ()
+@interface DetailViewController () <UIScrollViewDelegate>
+
+@property (nonatomic, strong) UIScrollView *scrollView;
 
 @end
 
 @implementation DetailViewController
+
+#pragma mark -- Visual Format Language Constants
+
+NSString *const kStationLabelHorizontal = @"H:|-[_stationLabel]-|";
+NSString *const kStationLabelVertical = @"V:|-[_stationLabel]|";
+NSString *const kBoroughLabelHorizontal = @"H:|-[_boroughLabel]-|";
+NSString *const kBoroughLabelVertical = @"V:|[_boroughLabel]-|";
+NSString *const kTrainLabelHorizontal = @"H:|-[_trainLabel]-|";
+NSString *const kTrainLabelVertical = @"V:|-[_trainLabel]-|";
+NSString *const kTypeLabelHorizontal = @"H:|-[_typeLabel]-|";
+NSString *const kTypeLabelVertical = @"V:|-[_typeLabel]-|";
+NSString *const kServingLabelHorizontal = @"H:|-[_servingLabel]-|";
+NSString *const kServingLabelVertical = @"V:|-[_servingLabel]-|";
+NSString *const kOutageDateLabelHorizontal = @"H:|-[_outageDateLabel]-|";
+NSString *const kOutageDateLabelVertical = @"V:|-[_outageDateLabel]-|";
+NSString *const kExpectedReturnToServiceLabelHorizontal = @"H:|-[_expectedReturnToServiceLabel]-|";
+NSString *const kExpectedReturnToServiceLabelVertical = @"V:|[_expectedReturnToServiceLabel]-100-|";
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -28,12 +47,27 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    CGRect stationLabelFrame = CGRectMake(0.0f, 0.0f, 100.0f, 24.0f);
+    self.scrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
+    self.scrollView.delegate = self;
+    UIView *innerView = [[UIView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, 960, 640)];
+    CGRect stationLabelFrame = CGRectMake(0.0f, 0.0f, 320.0f, 200.0f);
     self.stationLabel = [[UILabel alloc] initWithFrame:stationLabelFrame];
-    self.stationLabel.text = @"Station: ";
+    CGRect boroughLabelFrame = CGRectMake(300.0f, 200.0f, 300.0f, 200.0f);
+    self.boroughLabel = [[UILabel alloc] initWithFrame:boroughLabelFrame];
+    self.trainLabel = [[UILabel alloc] initWithFrame:stationLabelFrame];
+    self.typeLabel = [[UILabel alloc] initWithFrame:stationLabelFrame];
+    self.servingLabel = [[UILabel alloc] initWithFrame:stationLabelFrame];
+    self.outageDateLabel = [[UILabel alloc] initWithFrame:stationLabelFrame];
+    self.expectedReturnToServiceLabel = [[UILabel alloc] initWithFrame:stationLabelFrame];
+    
     self.stationLabel.font = [UIFont boldSystemFontOfSize:14.0f];
-    self.stationLabel.center = self.view.center;
-    [self.view addSubview:self.stationLabel];
+    self.stationLabel.center = innerView.center;
+    [self.view addSubview:self.scrollView];
+    [self.scrollView addSubview:self.stationLabel];
+    [self.scrollView addSubview:self.boroughLabel];
+    [self.scrollView addSubview:self.trainLabel];
+    self.stationLabel.text = self.xmlData.station;
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -41,16 +75,5 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
