87f57eab35051e7df668f3220748519c4f171f15
Michael Vilabrera <m.vilabrera@gmail.com>
2015-01-28 15:30:57 -0500

    cleaned up source files

--- NycSee/Controller/ServiceViewController.h
+++ NycSee/Controller/ServiceViewController.h
@@ -8,15 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-#import "XMLServiceData.h"
-
 @interface ServiceViewController : UIViewController
-
-//@property (strong, nonatomic) XMLServiceData *xmlServiceData;
-//@property (strong, nonatomic) UILabel *nameLabel;
-//@property (strong, nonatomic) UILabel *statusLabel;
-//@property (strong, nonatomic) UILabel *textLabel;
-//@property (strong, nonatomic) UILabel *dateLabel;
-//@property (strong, nonatomic) UILabel *timeLabel;
 
 @end
--- NycSee/Controller/ServiceViewController.m
+++ NycSee/Controller/ServiceViewController.m
@@ -24,6 +24,7 @@
     
     self.textView = [[UITextView alloc] initWithFrame:self.view.bounds];
     self.textView.editable = NO;
+    self.automaticallyAdjustsScrollViewInsets = NO;
     
     self.url = [[NSURL alloc] initWithString:SERVICE_STATUS_URL];
     NSError *error = [[NSError alloc] init];
@@ -52,22 +53,12 @@
     modifiedTemp = [modifiedTemp stringByReplacingOccurrencesOfString:@"Â" withString:@""];
     modifiedTemp = [modifiedTemp stringByReplacingOccurrencesOfString:@"<text />" withString:@"<br/><br/>"];
     modifiedTemp = [modifiedTemp stringByReplacingOccurrencesOfString:@"Show Reroute Details" withString:@""];
-    //    modifiedTemp = [modifiedTemp stringByReplacingOccurrencesOfString:@"<P>" withString:@""];
-    //    modifiedTemp = [modifiedTemp stringByReplacingOccurrencesOfString:@"</P>" withString:@""];
-    //    modifiedTemp = [modifiedTemp stringByReplacingOccurrencesOfString:@"<span class=\"" withString:@""];
-    //    modifiedTemp = [modifiedTemp stringByReplacingOccurrencesOfString:@"\">" withString:@""];
-    //    modifiedTemp = [modifiedTemp stringByReplacingOccurrencesOfString:@"</span>" withString:@""];
-    // finally, remove dead weight 'Title', 'Date', TitleServiceChange" >
-    //    modifiedTemp = [modifiedTemp stringByReplacingOccurrencesOfString:@"TitleDelay" withString:@""];
-    //    modifiedTemp = [modifiedTemp stringByReplacingOccurrencesOfString:@"DateStyle" withString:@""];
-    //    modifiedTemp = [modifiedTemp stringByReplacingOccurrencesOfString:@"TitleServiceChange\" >" withString:@""];
-    //    modifiedTemp = [modifiedTemp stringByReplacingOccurrencesOfString:@"TitlePlannedWork\" >" withString:@""];
+    
     NSLog(@"%@", modifiedTemp);     // gives desired output
     
     NSDictionary *options = @{NSDocumentTypeDocumentAttribute:NSHTMLTextDocumentType};
     
     NSData *completedData = [modifiedTemp dataUsingEncoding:NSUTF8StringEncoding]; // necessary for completion
-//    [self.webView loadData:completedData MIMEType:nil textEncodingName:nil baseURL:self.url];
     
     NSAttributedString *attributedString = [[NSAttributedString alloc] initWithData:completedData options:options documentAttributes:nil error:nil];
     self.textView.attributedText = attributedString;
@@ -75,70 +66,6 @@
     return completedData;
 }
 
-
-//#pragma mark -- Visual Format Language Constants
-//
-//NSString *const kNameLabelHorizontal = @"H:|-[_nameLabel]-|";
-//NSString *const kNameLabelVertical = @"V:|-[_nameLabel]-|";
-//NSString *const kStatusLabelHorizontal = @"H:|-[_statusLabel]-|";
-//NSString *const kStatusLabelVertical = @"V:|-[_statusLabel]-|";
-//NSString *const kTextLabelHorizontal = @"H:|-[_textLabel]-|";
-//NSString *const kTextLabelVertical = @"V:|-[_textLabel]|";
-//NSString *const kDateLabelHorizontal = @"H:|-[_dateLabel]-|";
-//NSString *const kDateLabelVertical = @"V:|-[_dateLabel]|";
-//NSString *const kTimeLabelHorizontal = @"H:|-[_timeLabel]-|";
-//NSString *const kTimeLabelVertical = @"V:|-[_timeLabel]|";
-//
-//double const kXCoordinateServicePage = 20.0f;
-//double const kFrameWidthServicePage = 280.0f;
-//double const kFrameHeightLesserServicePage = 20.0f;
-//double const kFrameHeightGreaterServicePage = 40.0f;
-//
-//- (id) initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-//{
-//    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-//    if (self) {
-//        // Custom initialization
-//        self.title = @"Service Changes";
-//        self.tabBarItem.image = [UIImage imageNamed:@"wrench"];
-//    }
-//    return self;
-//}
-//
-//- (void)viewDidLoad {
-//    [super viewDidLoad];
-//    // Do any additional setup after loading the view.
-//    self.title = @"Service Changes";
-//    CGRect nameLabelFrame = CGRectMake(kXCoordinateServicePage, 70.0f, kFrameWidthServicePage, kFrameHeightGreaterServicePage);
-//    CGRect statusLabelFrame = CGRectMake(kXCoordinateServicePage, 100.0f, kFrameWidthServicePage, kFrameHeightGreaterServicePage);
-//    CGRect textLabelFrame = CGRectMake(kXCoordinateServicePage, 130.0f, kFrameWidthServicePage, kFrameHeightGreaterServicePage);
-//    CGRect dateLabelFrame = CGRectMake(kXCoordinateServicePage, 200.0f, kFrameWidthServicePage, kFrameHeightGreaterServicePage);
-//    CGRect timeLabelFrame = CGRectMake(kXCoordinateServicePage, 230.0f, kFrameWidthServicePage, kFrameHeightGreaterServicePage);
-//    
-//    self.nameLabel = [[UILabel alloc] initWithFrame:nameLabelFrame];
-//    self.statusLabel = [[UILabel alloc] initWithFrame:statusLabelFrame];
-//    self.textLabel = [[UILabel alloc] initWithFrame:textLabelFrame];
-//    self.dateLabel = [[UILabel alloc] initWithFrame:dateLabelFrame];
-//    self.timeLabel = [[UILabel alloc] initWithFrame:timeLabelFrame];
-//    
-//    self.nameLabel.text = self.xmlServiceData.name;
-//    self.statusLabel.text = self.xmlServiceData.status;
-//    self.textLabel.text = self.xmlServiceData.text;
-//    self.dateLabel.text = self.xmlServiceData.date;
-//    self.timeLabel.text = self.xmlServiceData.time;
-//    
-//    self.textLabel.lineBreakMode = NSLineBreakByWordWrapping;
-//    self.textLabel.numberOfLines = 3;
-//    
-//    [self.view addSubview:self.nameLabel];
-//    [self.view addSubview:self.statusLabel];
-//    [self.view addSubview:self.textLabel];
-//    [self.view addSubview:self.dateLabel];
-//    [self.view addSubview:self.timeLabel];
-//    
-//    self.view.backgroundColor = [UIColor whiteColor];
-//}
-//
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
