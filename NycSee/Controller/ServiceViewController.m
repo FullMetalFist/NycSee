2c140094e5f9e8daf1e65936c8cdb7cedbbd750d
Michael Vilabrera <m.vilabrera@gmail.com>
2015-02-26 12:31:56 -0500

    assist from CG

--- NycSee/Controller/ServiceViewController.m
+++ NycSee/Controller/ServiceViewController.m
@@ -31,41 +31,58 @@
     self.automaticallyAdjustsScrollViewInsets = NO;
     
     self.url = [[NSURL alloc] initWithString:SERVICE_STATUS_URL];
-    NSError *error = [[NSError alloc] init];
-    NSData *data = [[NSData alloc] initWithContentsOfURL:self.url options:NSISOLatin1StringEncoding error:&error];
-    data = [self replaceHTMLentities:data];
     
-    [self.view addSubview:self.textView];
+    NSOperationQueue *backgroundQueue = [[NSOperationQueue alloc] init];
+    [backgroundQueue addOperationWithBlock:^{
+        
+        NSData *data = [[NSData alloc] initWithContentsOfURL:self.url options:NSISOLatin1StringEncoding error:nil];
+        data = [self replaceHTMLentities:data];
+
+        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+            [self.view addSubview:self.textView];
+        }];
+    }];       // getting 0xbbadbeef when dispatch_async is run
+
+//     NSData *data = [[NSData alloc] initWithContentsOfURL:self.url options:NSISOLatin1StringEncoding error:nil];
+//    data = [self replaceHTMLentities:data];
 }
 
 - (NSData *) replaceHTMLentities:(NSData *)data
 {
-    NSString *htmlCode = [[NSString alloc] initWithData:data encoding:NSISOLatin1StringEncoding];
-    NSMutableString *temp = [NSMutableString stringWithString:htmlCode];
-    
-    NSString *modifiedTemp = [NSString string];
-    modifiedTemp = [temp stringByReplacingOccurrencesOfString:@"&lt;" withString:@"<"];
-    modifiedTemp = [modifiedTemp stringByReplacingOccurrencesOfString:@"&gt;" withString:@">"];
-    modifiedTemp = [modifiedTemp stringByReplacingOccurrencesOfString:@"&amp;" withString:@"&"];
-    modifiedTemp = [modifiedTemp stringByReplacingOccurrencesOfString:@"&nbsp;" withString:@" "];
-    
-    // lets take this a bit further and remove <STRONG>,</STRONG>,<br/>,<P>,</P> tags.
-    // Â will not disappear
-    // remove <text />,Show Reroute Details
-    modifiedTemp = [modifiedTemp stringByReplacingOccurrencesOfString:@"<STRONG>" withString:@""];
-    modifiedTemp = [modifiedTemp stringByReplacingOccurrencesOfString:@"</STRONG>" withString:@""];
-    modifiedTemp = [modifiedTemp stringByReplacingOccurrencesOfString:@"Â" withString:@""];
-    modifiedTemp = [modifiedTemp stringByReplacingOccurrencesOfString:@"<text />" withString:@"<br/><br/>"];
-    modifiedTemp = [modifiedTemp stringByReplacingOccurrencesOfString:@"Show Reroute Details" withString:@""];
-    
-    NSDictionary *options = @{NSDocumentTypeDocumentAttribute:NSHTMLTextDocumentType};
-    
-    NSData *completedData = [modifiedTemp dataUsingEncoding:NSISOLatin1StringEncoding]; // necessary for completion
-    
-    NSAttributedString *attributedString = [[NSAttributedString alloc] initWithData:completedData options:options documentAttributes:nil error:nil];
-    self.textView.attributedText = attributedString;
-    
-    return completedData;
+    if (data) {
+        NSString *htmlCode = [[NSString alloc] initWithData:data encoding:NSISOLatin1StringEncoding];
+        
+        NSMutableString *temp = [NSMutableString stringWithString:htmlCode];
+        
+        NSString *modifiedTemp = [NSString string];
+        modifiedTemp = [temp stringByReplacingOccurrencesOfString:@"&lt;" withString:@"<"];
+        modifiedTemp = [modifiedTemp stringByReplacingOccurrencesOfString:@"&gt;" withString:@">"];
+        modifiedTemp = [modifiedTemp stringByReplacingOccurrencesOfString:@"&amp;" withString:@"&"];
+        modifiedTemp = [modifiedTemp stringByReplacingOccurrencesOfString:@"&nbsp;" withString:@" "];
+        
+        // lets take this a bit further and remove <STRONG>,</STRONG>,<br/>,<P>,</P> tags.
+        // Â will not disappear
+        // remove <text />,Show Reroute Details
+        modifiedTemp = [modifiedTemp stringByReplacingOccurrencesOfString:@"<STRONG>" withString:@""];
+        modifiedTemp = [modifiedTemp stringByReplacingOccurrencesOfString:@"</STRONG>" withString:@""];
+        modifiedTemp = [modifiedTemp stringByReplacingOccurrencesOfString:@"Â" withString:@""];
+        modifiedTemp = [modifiedTemp stringByReplacingOccurrencesOfString:@"<text />" withString:@"<br/><br/>"];
+        modifiedTemp = [modifiedTemp stringByReplacingOccurrencesOfString:@"Show Reroute Details" withString:@""];
+        
+        NSDictionary *options = @{NSDocumentTypeDocumentAttribute:NSHTMLTextDocumentType};
+        
+        NSData *completedData = [modifiedTemp dataUsingEncoding:NSISOLatin1StringEncoding]; // necessary for completion
+        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+            
+            NSAttributedString *attributedString = [[NSAttributedString alloc] initWithData:completedData options:options documentAttributes:nil error:nil];
+            self.textView.attributedText = attributedString;
+        }];
+        
+        return completedData;
+    }
+    else {
+        return nil;
+    }
 }
 
 - (void)didReceiveMemoryWarning {
