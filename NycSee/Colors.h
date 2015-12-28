e66b7539e9402ec249fbbb99b93dabf31ea7b9a1
Michael Vilabrera <m.vilabrera@gmail.com>
2015-12-27 22:56:22 -0500

    hudson yards location

---
+++ NycSee/Colors.h
@@ -0,0 +1,24 @@
+//
+//  Colors.h
+//  NycSee
+//
+//  Created by Michael Vilabrera on 11/7/15.
+//  Copyright © 2015 Giving Tree. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Colors : NSObject
+
++ (UIColor *)ACEColors;
++ (UIColor *)BDFMColors;
++ (UIColor *)GColor;
++ (UIColor *)JZColors;
++ (UIColor *)LColor;
++ (UIColor *)NQRColors;
++ (UIColor *)SColor;
++ (UIColor *)OneTwoThreeColors;
++ (UIColor *)FourFiveSixColors;
++ (UIColor *)SevenColor;
+
+@end
---
+++ NycSee/Colors.m
@@ -0,0 +1,53 @@
+//
+//  Colors.m
+//  NycSee
+//
+//  Created by Michael Vilabrera on 11/7/15.
+//  Copyright © 2015 Giving Tree. All rights reserved.
+//
+
+#import "Colors.h"
+
+@implementation Colors
+
++ (UIColor *)ACEColors {
+    return [UIColor colorWithRed:0.000f green:0.224f blue:0.651f alpha:1.00f];
+}
+
++ (UIColor *)BDFMColors {
+    return [UIColor colorWithRed:1.000f green:0.388f blue:0.098f alpha:1.00f];
+}
+
++ (UIColor *)GColor {
+    return [UIColor colorWithRed:0.424f green:0.745f blue:0.271f alpha:1.00f];
+}
+
++ (UIColor *)JZColors {
+    return [UIColor colorWithRed:0.600f green:0.400f blue:0.200f alpha:1.00f];
+}
+
++ (UIColor *)LColor {
+    return [UIColor colorWithRed:0.655f green:0.663f blue:0.675f alpha:1.00f];
+}
+
++ (UIColor *)NQRColors {
+    return [UIColor colorWithRed:0.988f green:0.800f blue:0.039f alpha:1.00f];
+}
+
++ (UIColor *)SColor {
+    return [UIColor colorWithRed:0.502f green:0.506f blue:0.514f alpha:1.00f];
+}
+
++ (UIColor *)OneTwoThreeColors {
+    return [UIColor colorWithRed:0.933f green:0.208f blue:0.180f alpha:1.00f];
+}
+
++ (UIColor *)FourFiveSixColors {
+    return [UIColor colorWithRed:0.000f green:0.576f blue:0.235f alpha:1.00f];
+}
+
++ (UIColor *)SevenColor {
+    return [UIColor colorWithRed:0.725f green:0.200f blue:0.678f alpha:1.00f];
+}
+
+@end
