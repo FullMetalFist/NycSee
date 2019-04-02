9b90fca935d808443646cb6d26228b9fcf928d0c
Michael Vilabrera <m.vilabrera@gmail.com>
2019-04-01 22:09:56 -0400

    stuff

---
+++ NycSee/View/FindMeButton.h
@@ -0,0 +1,13 @@
+//
+//  FindMeButton.h
+//  NycSee
+//
+//  Created by Michael Vilabrera on 7/5/18.
+//  Copyright © 2018 Giving Tree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FindMeButton : UIButton
+
+@end
---
+++ NycSee/View/FindMeButton.m
@@ -0,0 +1,21 @@
+//
+//  FindMeButton.m
+//  NycSee
+//
+//  Created by Michael Vilabrera on 7/5/18.
+//  Copyright © 2018 Giving Tree. All rights reserved.
+//
+
+#import "FindMeButton.h"
+
+@implementation FindMeButton
+
+- (void) setHighlighted:(BOOL)highlighted {
+    [super setHighlighted:highlighted];
+    
+    if (highlighted) {
+//        self.backgroundColor = UIColorFromRGB();
+    }
+}
+
+@end
